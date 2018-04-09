23f728591e0d7310dc598e63f5d1252a043696e4
Rodrigo Pereira <rodrigo_pereira@outlook.com>
2018-04-09 06:07:41 -0300

    feat(Welcome): create initial page component

---
+++ src/container/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "./Welcome.css";
+
+const Welcome = () => {
+  return <div className="initial">
+    <h2>Teste para VML.</h2>
+    <p>Clique em um repositório no menu lateral para saber mais detalhes.</p>
+  </div>
+};
+
+export default Welcome;
